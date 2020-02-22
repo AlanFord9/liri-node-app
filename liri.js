cd520d61975082d956b0b4bade2e9545389846ae
frankcodes <alanforducf@gmail.com>
2020-02-21 19:49:22 -0500

    spotify call for specific track complete

--- liri.js
+++ liri.js
@@ -1,5 +1,33 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var moment = require('moment')
+var nodeArg = process.argv
+var command = nodeArg[2]
+var userInput = nodeArg.slice(3).join(" ");
+
+
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+if (command === "spotify-this-song") {
+
+    // TODO Create default search if no song found for "The Sign" by Ace of Base.
+spotify.search({ type: 'track', query: userInput }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    //   console.log(JSON.stringify(data, null, 2));
+
+    var songData = data.tracks.items[0];
+    console.log("Artist: " + songData.artists[0].name);
+    console.log("Song Title: " + songData.name);
+    console.log("Preview Track: " + songData.preview_url);
+    console.log("Album: " + songData.album.name);
+
+  });
+
+}
