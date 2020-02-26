3b16b2cd2ec8d9e3d9ffaa70ee36e4b66ecc0f8f
frankcodes <alanforducf@gmail.com>
2020-02-25 21:53:27 -0500

    project complete

--- liri.js
+++ liri.js
@@ -2,42 +2,53 @@ require("dotenv").config();
 
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
-var moment = require('moment')
+var moment = require('moment');
 var fs = require("fs");
-var nodeArg = process.argv
-var command = nodeArg[2]
+var nodeArg = process.argv;
+var command = nodeArg[2];
 var userInput = nodeArg.slice(3).join(" ");
-var movieU = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
-var bandU = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
-
-
-
+var movieU = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+var bandU = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+var moment = require('moment');
+moment().format();
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
 
-if (command === "spotify-this-song") {
-
-    // TODO Create default search if no song found for "The Sign" by Ace of Base.
-    spotify.search({ type: 'track', query: userInput }, function(err, data) {
+function songSearch(command, userInput) {
+    if (!userInput) {
+    spotify.search({ type: 'track', query: "the sign" }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        var songData = data.tracks.items;
 
-    //   console.log(JSON.stringify(data, null, 2));
+        console.log("Artist: " + songData[0].artists[0].name + "\nSong Title: " + songData[0].name +
+        "\nPreview Track: " + songData[0].preview_url + "\nAlbum: " + songData[0].album.name);
 
-    var songData = data.tracks.items[0];
-    console.log("Artist: " + songData.artists[0].name);
-    console.log("Song Title: " + songData.name);
-    console.log("Preview Track: " + songData.preview_url);
-    console.log("Album: " + songData.album.name);
+    })} 
+    else {
+        spotify.search({ type: 'track', query: userInput }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
 
-  });
+            var songData = data.tracks.items;
 
-    // TODO Create default search if no movie is entered to output data for the movie 'Mr. Nobody.'
-} else if (command === "movie-this") {
+            for(s = 0; s < songData.length; s++)
 
-    axios.get(movieU).then(
+            console.log("Artist: " + songData[s].artists[0].name + "\nSong Title: " + songData[s].name +
+            "\nPreview Track: " + songData[s].preview_url + "\nAlbum: " + songData[s].album.name + "\n-------------------------");
+        });
+    }
+}
+
+function movieSearch(userInput) {
+    movieU = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
+    movieU2 = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+
+    if (!userInput) {
+    axios.get(movieU2).then(
+
         function(response) {
             var movieData = response.data
             console.log(movieData.Title);
@@ -51,8 +62,6 @@ if (command === "spotify-this-song") {
         })
         .catch(function(error) {
             if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
             console.log("---------------Data---------------");
             console.log(error.response.data);
             console.log("---------------Status---------------");
@@ -60,21 +69,66 @@ if (command === "spotify-this-song") {
             console.log("---------------Status---------------");
             console.log(error.response.headers);
             } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+
             console.log(error.request);
             } else {
-            // Something happened in setting up the request that triggered an Error
             console.log("Error", error.message);
             }
             console.log(error.config);
         });
+    } 
+    else {
+        axios.get(movieU).then(
 
-} else if (command === "concert-this") {
+            function(response) {
+                var movieData = response.data
+                console.log(movieData.Title);
+                console.log(movieData.Year);
+                console.log(movieData.imdbRating);
+                console.log(movieData.Ratings[1].Source + " gave this movie a rating of " + movieData.Ratings[1].Value);
+                console.log(movieData.Country);
+                console.log(movieData.Language);
+                console.log(movieData.Plot);
+                console.log(movieData.Actors);
+            })
+            .catch(function(error) {
+                if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+                } 
+                else if (error.request) {
+                console.log(error.request);
+                } 
+                else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+    }
+} 
+
+function concertSearch() {
 
     axios.get(bandU).then(
         function(response) {
-            console.log(response)
+
+            var concertInfo = response.data
+
+            for(c = 0; c < concertInfo.length; c++) {
+            var concertDate = concertInfo[c].datetime
+            var dateFormat = "YYYY-MM-DD"
+            var convertedDate = moment(concertDate, dateFormat);
+
+            console.log("Venue Name: " + concertInfo[c].venue.name + "\nCity/State: " + concertInfo[c].venue.city + ", " + concertInfo[c].venue.region +
+            "\nDate: " + convertedDate.format("MM/DD/YYYY") + "\n-------------------------");
+            }
         })
         .catch(function(error) {
             if (error.response) {
@@ -96,14 +150,48 @@ if (command === "spotify-this-song") {
             }
             console.log(error.config);
         });
-} else if (command === "do-what-it-says") {
+
+} 
+
+function readTxt() {
 
     fs.readFile("random.txt", "utf8", function(error, data) {
 
         if (error) {
           return console.log(error);
         }
-        console.log(data);
-      
+        // console.log(data);
+        txtSearch = data.split(",");
+        // console.log(txtSearch)
+        songSearch(txtSearch[0], txtSearch[1]);
       });
 }
+
+function runSwitch(command, userInput) {
+
+    switch (command) {
+        case "spotify-this-song":
+          songSearch(command, userInput);
+          break;
+        
+        case "movie-this":
+          movieSearch(userInput);
+          break;
+        
+        case "concert-this":
+          concertSearch(userInput);
+          break;
+        
+        case "do-what-it-says":
+          readTxt(command, userInput);
+          break;
+        default:
+            console.log("idk bro")
+        }
+}
+
+function pickCommand(command, userInput) {
+        runSwitch(command, userInput)
+}
+
+pickCommand(command, userInput)
